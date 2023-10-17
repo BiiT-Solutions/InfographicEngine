f05ead3126290af44839c78098591640ddc104f9
Jorge Hortelano <jhortelano@biit-solutions.com>
2023-10-17 16:58:40 +0200

    Added more tests for find endpoints

--- infographic-engine-rest/src/test/java/com/biit/infographic/rest/tests/InfographicRestTests.java
+++ infographic-engine-rest/src/test/java/com/biit/infographic/rest/tests/InfographicRestTests.java
@@ -1,5 +1,7 @@
 package com.biit.infographic.rest.tests;
 
+import com.biit.infographic.core.controllers.GeneratedInfographicController;
+import com.biit.infographic.core.models.GeneratedInfographicDTO;
 import com.biit.server.security.AuthenticatedUserProvider;
 import com.biit.server.security.model.AuthRequest;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -18,12 +20,16 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.context.WebApplicationContext;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -40,6 +46,8 @@ public class InfographicRestTests extends AbstractTestNGSpringContextTests {
     private final static String USER_PASSWORD = "password";
 
     private final static String JWT_SALT = "4567";
+    private final static String FORM_NAME = "CADT";
+    private final static int FORM_VERSION = 1;
 
     @Autowired
     private WebApplicationContext context;
@@ -52,6 +60,9 @@ public class InfographicRestTests extends AbstractTestNGSpringContextTests {
 
     @Autowired
     private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private GeneratedInfographicController generatedInfographicController;
 
     private MockMvc mockMvc;
 
@@ -72,17 +83,20 @@ public class InfographicRestTests extends AbstractTestNGSpringContextTests {
                 .build();
     }
 
+
     @BeforeClass(dependsOnMethods = "setUp")
     public void addUser() {
         //Create the admin user
         authenticatedUserProvider.createUser(USER_NAME, USER_NAME, USER_PASSWORD);
     }
 
+
     @Test
     public void checkAuthentication() {
         //Check the admin user
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(USER_NAME, JWT_SALT + USER_PASSWORD));
     }
+
 
     @Test
     public void setAuthentication() throws Exception {
@@ -103,6 +117,7 @@ public class InfographicRestTests extends AbstractTestNGSpringContextTests {
         Assert.assertNotNull(jwtToken);
     }
 
+
     @Test
     public void checkPublicRestService() throws Exception {
         //Info services are opened in rest-server library
@@ -112,15 +127,253 @@ public class InfographicRestTests extends AbstractTestNGSpringContextTests {
                 .andExpect(content().string(""));
     }
 
+
     //NO data, 404 returned.
     @Test(dependsOnMethods = "setAuthentication")
-    public void getLatestInfographic() throws Exception {
+    public void getLatestInfographicWithoutData() throws Exception {
+        MultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+        requestParams.add("form", FORM_NAME);
+        requestParams.add("version", String.valueOf(FORM_VERSION));
+        requestParams.add("createdBy", USER_NAME);
+
         mockMvc.perform(get("/infographic/find/latest")
                         .header(HttpHeaders.AUTHORIZATION,
                                 "Bearer " + jwtToken)
                         .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .params(requestParams)
                         .with(csrf()))
                 .andExpect(MockMvcResultMatchers.status().isNotFound())
                 .andReturn();
+    }
+
+
+    @Test(dependsOnMethods = "getLatestInfographicWithoutData")
+    public void populateDatabase() {
+        final GeneratedInfographicDTO generatedInfographic = new GeneratedInfographicDTO();
+        generatedInfographic.setFormName(FORM_NAME);
+        generatedInfographic.setFormVersion(FORM_VERSION);
+        generatedInfographicController.create(generatedInfographic, USER_NAME);
+
+
+        final GeneratedInfographicDTO generatedInfographic2 = new GeneratedInfographicDTO();
+        generatedInfographic2.setFormName(FORM_NAME + "_bad");
+        generatedInfographic2.setFormVersion(FORM_VERSION);
+        generatedInfographicController.create(generatedInfographic2, USER_NAME);
+
+        final GeneratedInfographicDTO generatedInfographic3 = new GeneratedInfographicDTO();
+        generatedInfographic3.setFormName(FORM_NAME);
+        generatedInfographic3.setFormVersion(FORM_VERSION);
+        generatedInfographicController.create(generatedInfographic3, USER_NAME + "_bad");
+
+        final GeneratedInfographicDTO generatedInfographic4 = new GeneratedInfographicDTO();
+        generatedInfographic4.setFormName(FORM_NAME);
+        generatedInfographic4.setFormVersion(FORM_VERSION + 1);
+        generatedInfographicController.create(generatedInfographic4, USER_NAME);
+    }
+
+
+    @Test(dependsOnMethods = "populateDatabase")
+    public void getLatestInfographicWithData() throws Exception {
+        MultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+        requestParams.add("form", FORM_NAME);
+        requestParams.add("version", String.valueOf(FORM_VERSION));
+        requestParams.add("createdBy", USER_NAME);
+
+        mockMvc.perform(get("/infographic/find/latest")
+                        .header(HttpHeaders.AUTHORIZATION,
+                                "Bearer " + jwtToken)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .params(requestParams)
+                        .with(csrf()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+    }
+
+
+    @Test(dependsOnMethods = "populateDatabase")
+    public void getLatestInfographicWithDataInvalidVersion() throws Exception {
+        MultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+        requestParams.add("form", FORM_NAME);
+        requestParams.add("version", "500");
+        requestParams.add("createdBy", USER_NAME);
+
+        mockMvc.perform(get("/infographic/find/latest")
+                        .header(HttpHeaders.AUTHORIZATION,
+                                "Bearer " + jwtToken)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .params(requestParams)
+                        .with(csrf()))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andReturn();
+    }
+
+    @Test(dependsOnMethods = "populateDatabase")
+    public void getLatestInfographicWithDataInvalidUser() throws Exception {
+        MultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+        requestParams.add("form", FORM_NAME);
+        requestParams.add("version", String.valueOf(FORM_VERSION));
+        requestParams.add("createdBy", USER_NAME + "_not_me");
+
+        mockMvc.perform(get("/infographic/find/latest")
+                        .header(HttpHeaders.AUTHORIZATION,
+                                "Bearer " + jwtToken)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .params(requestParams)
+                        .with(csrf()))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andReturn();
+    }
+
+    @Test(dependsOnMethods = "populateDatabase")
+    public void getLatestInfographicWithDataInvalidFormName() throws Exception {
+        MultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+        requestParams.add("form", FORM_NAME + "_incorrect");
+        requestParams.add("version", String.valueOf(FORM_VERSION));
+        requestParams.add("createdBy", USER_NAME);
+
+        mockMvc.perform(get("/infographic/find/latest")
+                        .header(HttpHeaders.AUTHORIZATION,
+                                "Bearer " + jwtToken)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .params(requestParams)
+                        .with(csrf()))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andReturn();
+    }
+
+
+    @Test(dependsOnMethods = "populateDatabase")
+    public void getAllInfographicWithData() throws Exception {
+        MultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+        requestParams.add("form", FORM_NAME);
+        requestParams.add("version", String.valueOf(FORM_VERSION));
+        requestParams.add("createdBy", USER_NAME);
+
+        MvcResult createResult = mockMvc.perform(get("/infographic/find")
+                        .header(HttpHeaders.AUTHORIZATION,
+                                "Bearer " + jwtToken)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .params(requestParams)
+                        .with(csrf()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        final List<GeneratedInfographicDTO> results = Arrays.asList(objectMapper.readValue(createResult.getResponse().getContentAsString(), GeneratedInfographicDTO[].class));
+        Assert.assertEquals(results.size(), 1);
+    }
+
+
+    @Test(dependsOnMethods = "populateDatabase")
+    public void getAllInfographicByUser() throws Exception {
+        MultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+        requestParams.add("createdBy", USER_NAME);
+
+        MvcResult createResult = mockMvc.perform(get("/infographic/find")
+                        .header(HttpHeaders.AUTHORIZATION,
+                                "Bearer " + jwtToken)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .params(requestParams)
+                        .with(csrf()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        final List<GeneratedInfographicDTO> results = Arrays.asList(objectMapper.readValue(createResult.getResponse().getContentAsString(), GeneratedInfographicDTO[].class));
+        Assert.assertEquals(results.size(), 3);
+    }
+
+
+    @Test(dependsOnMethods = "populateDatabase")
+    public void getAllInfographicByFormName() throws Exception {
+        MultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+        requestParams.add("form", FORM_NAME);
+
+        MvcResult createResult = mockMvc.perform(get("/infographic/find")
+                        .header(HttpHeaders.AUTHORIZATION,
+                                "Bearer " + jwtToken)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .params(requestParams)
+                        .with(csrf()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        final List<GeneratedInfographicDTO> results = Arrays.asList(objectMapper.readValue(createResult.getResponse().getContentAsString(), GeneratedInfographicDTO[].class));
+        Assert.assertEquals(results.size(), 3);
+    }
+
+
+    @Test(dependsOnMethods = "populateDatabase")
+    public void getAllInfographicByVersion() throws Exception {
+        MultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+        requestParams.add("version", String.valueOf(FORM_VERSION));
+
+        MvcResult createResult = mockMvc.perform(get("/infographic/find")
+                        .header(HttpHeaders.AUTHORIZATION,
+                                "Bearer " + jwtToken)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .params(requestParams)
+                        .with(csrf()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        final List<GeneratedInfographicDTO> results = Arrays.asList(objectMapper.readValue(createResult.getResponse().getContentAsString(), GeneratedInfographicDTO[].class));
+        Assert.assertEquals(results.size(), 3);
+    }
+
+
+    @Test(dependsOnMethods = "populateDatabase")
+    public void getAllInfographicByWrongVersion() throws Exception {
+        MultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+        requestParams.add("version", String.valueOf(3));
+
+        MvcResult createResult = mockMvc.perform(get("/infographic/find")
+                        .header(HttpHeaders.AUTHORIZATION,
+                                "Bearer " + jwtToken)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .params(requestParams)
+                        .with(csrf()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        final List<GeneratedInfographicDTO> results = Arrays.asList(objectMapper.readValue(createResult.getResponse().getContentAsString(), GeneratedInfographicDTO[].class));
+        Assert.assertEquals(results.size(), 0);
+    }
+
+
+    @Test(dependsOnMethods = "populateDatabase")
+    public void getAllInfographicByDate() throws Exception {
+        MultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+        requestParams.add("from", "2023-01-01T00:00:00.00Z");
+        requestParams.add("to", "2050-01-01T00:00:00.00Z");
+
+        MvcResult createResult = mockMvc.perform(get("/infographic/find")
+                        .header(HttpHeaders.AUTHORIZATION,
+                                "Bearer " + jwtToken)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .params(requestParams)
+                        .with(csrf()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        final List<GeneratedInfographicDTO> results = Arrays.asList(objectMapper.readValue(createResult.getResponse().getContentAsString(), GeneratedInfographicDTO[].class));
+        Assert.assertEquals(results.size(), 4);
+    }
+
+    @Test(dependsOnMethods = "populateDatabase")
+    public void getAllInfographicByWrongDate() throws Exception {
+        MultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+        requestParams.add("from", "2049-01-01T00:00:00.00Z");
+        requestParams.add("to", "2050-01-01T00:00:00.00Z");
+
+        MvcResult createResult = mockMvc.perform(get("/infographic/find")
+                        .header(HttpHeaders.AUTHORIZATION,
+                                "Bearer " + jwtToken)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .params(requestParams)
+                        .with(csrf()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        final List<GeneratedInfographicDTO> results = Arrays.asList(objectMapper.readValue(createResult.getResponse().getContentAsString(), GeneratedInfographicDTO[].class));
+        Assert.assertEquals(results.size(), 0);
     }
 }
