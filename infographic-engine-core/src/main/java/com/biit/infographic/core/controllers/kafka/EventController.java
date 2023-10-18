0482be682ed74be07a5cd9a53cebe1b9bbd773f5
Jorge Hortelano <jhortelano@biit-solutions.com>
2023-10-18 10:14:54 +0200

    Corrected drools event read

--- infographic-engine-core/src/main/java/com/biit/infographic/core/controllers/kafka/EventController.java
+++ infographic-engine-core/src/main/java/com/biit/infographic/core/controllers/kafka/EventController.java
@@ -3,20 +3,25 @@ package com.biit.infographic.core.controllers.kafka;
 import com.biit.drools.form.DroolsSubmittedForm;
 import com.biit.infographic.core.controllers.DroolsResultController;
 import com.biit.infographic.core.controllers.kafka.converter.EventConverter;
+import com.biit.infographic.core.models.svg.serialization.ObjectMapperFactory;
 import com.biit.infographic.logger.EventsLogger;
 import com.biit.infographic.persistence.repositories.DroolsResultRepository;
 import com.biit.kafka.consumers.EventListener;
 import com.biit.kafka.events.Event;
 import com.biit.kafka.events.EventCustomProperties;
+import com.biit.kafka.events.EventPayload;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.stereotype.Controller;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.TimeZone;
 
 
 @Controller
 public class EventController {
+    private static final String FACT_TYPE = "DroolsResultForm";
 
     private final EventConverter eventConverter;
 
@@ -48,20 +53,36 @@ public class EventController {
                 : event.getCreatedBy();
 
         try {
+            if (event.getCustomProperties() != null) {
+                if (!Objects.equals(event.getCustomProperty(EventCustomProperties.FACT_TYPE), FACT_TYPE)) {
+                    EventsLogger.info(this.getClass(), "Event ignored.");
+                    return;
+                }
+            }
             final DroolsSubmittedForm droolsForm = getDroolsForm(event);
             EventsLogger.debug(this.getClass(), "Received Drools Result '{}'.", droolsForm.getName());
             droolsResultRepository.save(eventConverter.getDroolsContent(event, droolsForm));
             EventsLogger.debug(this.getClass(), "Drools Result '{}' saved.", droolsForm.getName());
             droolsResultController.process(droolsForm, createdBy);
+        } catch (JsonProcessingException e) {
+            EventsLogger.severe(this.getClass(), "Event cannot be parsed!!\n" + event);
         } catch (Exception e) {
             //Not a FormResult but maybe a SubmittedForm.
             EventsLogger.severe(this.getClass(), "Invalid event received!!\n" + event);
         }
     }
 
-
-    private DroolsSubmittedForm getDroolsForm(Event event) {
-        return event.getEntity(DroolsSubmittedForm.class);
+    private DroolsSubmittedForm getDroolsForm(Event event) throws JsonProcessingException {
+        return ObjectMapperFactory.getObjectMapper().readValue(getDroolsEventPayload(event).json, DroolsSubmittedForm.class);
     }
 
+
+    private DroolsSubmittedFormPayload getDroolsEventPayload(Event event) {
+        return event.getEntity(DroolsSubmittedFormPayload.class);
+    }
+
+
+    private static class DroolsSubmittedFormPayload implements EventPayload {
+        private String json;
+    }
 }
