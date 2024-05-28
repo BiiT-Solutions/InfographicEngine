8ee76e8653f1aede759fa14410a251de7abb9110
Jorge Hortelano <jhortelano@biit-solutions.com>
2024-05-28 17:40:43 +0200

    Chaning timezone to UTC

--- infographic-engine-core/src/main/java/com/biit/infographic/core/engine/content/AppointmentContent.java
+++ infographic-engine-core/src/main/java/com/biit/infographic/core/engine/content/AppointmentContent.java
@@ -14,6 +14,7 @@ import java.time.DateTimeException;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 import java.util.Set;
@@ -142,8 +143,8 @@ public class AppointmentContent {
         if (timezone != null && !timezone.isBlank()) {
             try {
                 InfographicEngineLogger.debug(this.getClass(), "Converting timezone from '{}' ({}) to '{}' ({}).",
-                        localDateTime, ZoneId.systemDefault(), localDateTime, ZoneId.of(timezone));
-                return localDateTime.atZone(ZoneId.systemDefault()).withZoneSameInstant(ZoneId.of(timezone)).format(DateTimeFormatter.ofPattern(TIME_FORMAT));
+                        localDateTime, ZoneOffset.UTC, localDateTime, ZoneId.of(timezone));
+                return localDateTime.atZone(ZoneOffset.UTC).withZoneSameInstant(ZoneId.of(timezone)).format(DateTimeFormatter.ofPattern(TIME_FORMAT));
             } catch (DateTimeException e) {
                 InfographicEngineLogger.severe(this.getClass(), "Invalid timezone provided '" + timezone + "'.");
                 InfographicEngineLogger.errorMessage(this.getClass(), e);
