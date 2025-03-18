82ed6608f1fdc77a6f9b86a87641c459db01b453
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-03-18 10:18:33 +0100

    Added 404 test

---
+++ infographic-engine-rest/src/test/java/com/biit/infographic/rest/tests/SvgTests.java
@@ -0,0 +1,190 @@
+package com.biit.infographic.rest.tests;
+
+import com.biit.infographic.core.controllers.GeneratedInfographicController;
+import com.biit.infographic.core.models.GeneratedInfographicDTO;
+import com.biit.infographic.core.providers.UserProvider;
+import com.biit.infographic.rest.api.model.InfographicSearch;
+import com.biit.server.security.model.AuthRequest;
+import com.biit.usermanager.client.providers.AuthenticatedUserProvider;
+import com.biit.utils.file.FileReader;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ExtendWith(MockitoExtension.class)
+@Test(groups = "svgTests")
+public class SvgTests extends AbstractTestNGSpringContextTests {
+
+    private static final String OUTPUT_FOLDER = System.getProperty("java.io.tmpdir") + File.separator + "SvgTests";
+
+    private static final String USER_NAME = "user";
+    private static final String USER_PASSWORD = "password";
+
+    private static final String JWT_SALT = "4567";
+    private static final String FORM_DOES_NOT_EXISTS = "Not Exists";
+    private static final int FORM_VERSION = 1;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private AuthenticatedUserProvider authenticatedUserProvider;
+
+    @Autowired
+    private UserProvider userProvider;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private GeneratedInfographicController generatedInfographicController;
+
+    private MockMvc mockMvc;
+
+    private String jwtToken;
+
+    private <T> String toJson(T object) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(object);
+    }
+
+    private <T> T fromJson(String payload, Class<T> clazz) throws IOException {
+        return objectMapper.readValue(payload, clazz);
+    }
+
+    private void checkContent(String content, String resourceFile) {
+        try {
+            Assert.assertEquals(content.trim(), Files.readString(Paths.get(getClass().getClassLoader()
+                    .getResource("svg" + File.separator + resourceFile).toURI())).trim());
+        } catch (IOException | URISyntaxException e) {
+            Assert.fail();
+        }
+    }
+
+    protected boolean deleteDirectory(File directoryToBeDeleted) {
+        File[] allContents = directoryToBeDeleted.listFiles();
+        if (allContents != null) {
+            for (File file : allContents) {
+                deleteDirectory(file);
+            }
+        }
+        return directoryToBeDeleted.delete();
+    }
+
+    @BeforeClass
+    public void prepareFolder() throws IOException {
+        Files.createDirectories(Paths.get(OUTPUT_FOLDER));
+    }
+
+    @BeforeClass
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(SecurityMockMvcConfigurers.springSecurity())
+                .build();
+    }
+
+
+    @BeforeClass(dependsOnMethods = "setUp")
+    public void addUser() {
+        //Create the admin user
+        authenticatedUserProvider.createUser(USER_NAME, USER_NAME, USER_PASSWORD);
+    }
+
+
+    @Test
+    public void checkAuthentication() {
+        //Check the admin user
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(USER_NAME, JWT_SALT + USER_PASSWORD));
+    }
+
+
+    @Test
+    public void setAuthentication() throws Exception {
+        AuthRequest request = new AuthRequest();
+        request.setUsername(USER_NAME);
+        request.setPassword(USER_PASSWORD);
+
+        MvcResult createResult = this.mockMvc
+                .perform(post("/auth/public/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toJson(request))
+                        .with(csrf()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.header().exists(HttpHeaders.AUTHORIZATION))
+                .andReturn();
+
+        jwtToken = createResult.getResponse().getHeader(HttpHeaders.AUTHORIZATION);
+        Assert.assertNotNull(jwtToken);
+    }
+
+
+
+    //NO data, 404 returned.
+    @Test(dependsOnMethods = "setAuthentication")
+    public void get404InByteService() throws Exception {
+        MultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+        requestParams.add("form", FORM_DOES_NOT_EXISTS);
+        requestParams.add("version", String.valueOf(FORM_VERSION));
+        requestParams.add("createdBy", USER_NAME);
+
+        mockMvc.perform(get("/svg/find/latest/pdf")
+                        .header(HttpHeaders.AUTHORIZATION,
+                                "Bearer " + jwtToken)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .params(requestParams)
+                        .with(csrf()))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andReturn();
+    }
+
+    @AfterClass
+    public void removeFolder() {
+        Assert.assertTrue(deleteDirectory(new File(OUTPUT_FOLDER)));
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void removeUser() {
+        authenticatedUserProvider.clear();
+        userProvider.reset();
+    }
+}
